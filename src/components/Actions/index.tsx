8720557b92b5fe112ec901bde531b4f7280e60a4
truongdinhduc-ecombest <duc-truong@ecombest.com>
2024-04-01 17:43:44 +0700

    First commit

--- src/components/Actions/index.tsx
+++ src/components/Actions/index.tsx
@@ -6,30 +6,14 @@ interface ActionsProps {
     label?: string;
     onClick?: (...args: any) => void;
   };
-  cancelButton?: {
-    label?: string;
-    onClick?: (...args: any) => void;
-  };
 }
 
 export default function ActionsButton(props: ActionsProps) {
-  const { confirmButton, cancelButton } = props;
+  const { confirmButton } = props;
+
   return (
     <div className={styles["actions"]}>
-      {/* <div className={styles["cancel"]} onClick={cancelButton?.onClick}>
-        {cancelButton?.label ?? "Cancel"}
-      </div> */}
       <div className={styles["confirm"]}>
-        {/* <div
-          className={
-            confirmButton?.onClick ? styles["original"] : styles["disable"]
-          }
-          onClick={() => {
-            confirmButton?.onClick?.(true);
-          }}
-        >
-          Skip
-        </div> */}
         <div
           className={
             confirmButton?.onClick ? styles["edited"] : styles["disable"]
